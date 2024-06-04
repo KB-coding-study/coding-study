d01c3da8578ecfb2058636dff1ac08d878d4d5c5
YeSeul <ysbsjh2464@naver.com>
2024-06-04 20:28:41 +0900

    2주차-이예슬-1874

---
+++ 2주차/yeseul/BJ1874.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+public class BJ1874 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+
+        Stack<Integer> stack = new Stack<>();
+        int tmp = 0;
+
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+
+            if (num > tmp) {
+                for (int j = tmp + 1; j <= num; j++) {
+                    stack.push(j);
+                    sb.append("+\n");
+                }
+                tmp = num;
+            }
+
+            if (stack.peek() == num) {
+                stack.pop();
+                sb.append("-\n");
+            }
+        }
+        if (stack.size() > 0) {
+            System.out.println("NO\n");
+        } else {
+            System.out.println(sb);
+        }
+    }
+}
