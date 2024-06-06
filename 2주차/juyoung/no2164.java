93ab5e61902e3bee33b858bd50dbdddd62c7c02e
kjy <gimjuyeong0620@gmail.com>
2024-06-07 08:55:34 +0900

    2주차-김주영-2164

---
+++ 2주차/juyoung/no2164.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+public class no2164 {
+
+    static Queue<Integer> q;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        q = new LinkedList<>();
+        for (int i = 1 ; i <= N ; i++) {
+            q.offer(i);
+        }
+
+        while (q.size() > 1) {
+            q.remove();
+            if(q.size() > 1) {
+                q.offer(q.poll());
+            }
+        }
+        System.out.println(q.poll());
+    }
+}
