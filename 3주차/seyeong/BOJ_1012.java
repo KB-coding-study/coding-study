7d84b0bcdb3b4eca16f15817be111683ba46d262
Hongse0 <hongseyoung981007@gmail.com>
2024-06-13 21:02:49 +0900

    3주차-홍세영-0613

---
+++ 3주차/seyeong/BOJ_1012.java
@@ -0,0 +1,59 @@
+package com.ohgiraffers;
+
+import java.util.Scanner;
+
+public class BOJ_1012 {
+    static int[][] field;
+    static boolean[][] visited;
+    static int[] dx = {0, 0, 1, -1};
+    static int[] dy = {1, -1, 0, 0};
+    static int M, N, K;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        for (int t = 0; t < T; t++) {
+            M = sc.nextInt();
+            N = sc.nextInt();
+            K = sc.nextInt();
+
+            field = new int[N][M];
+            visited = new boolean[N][M];
+
+            for (int i = 0; i < K; i++) {
+                int x = sc.nextInt();
+                int y = sc.nextInt();
+                field[y][x] = 1; // 배추 심기
+            }
+
+            int wormCount = 0;
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    if (field[i][j] == 1 && !visited[i][j]) {
+                        dfs(i, j);
+                        wormCount++;
+                    }
+                }
+            }
+
+            System.out.println(wormCount);
+        }
+        sc.close();
+    }
+
+    public static void dfs(int x, int y) {
+        visited[x][y] = true;
+
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx >= 0 && ny >= 0 && nx < N && ny < M) {
+                if (field[nx][ny] == 1 && !visited[nx][ny]) {
+                    dfs(nx, ny);
+                }
+            }
+        }
+    }
+}
