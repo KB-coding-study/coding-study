fd2c4fb9acb93ac1c18a68e431bd7c14d4c07159
bongj9 <lollypark501@naver.com>
2024-06-30 03:11:04 +0900

    5주차_장봉준_1967_미완성

---
+++ 5주차/bongJ/BOJ1967.java
@@ -0,0 +1,16 @@
+package bongJ;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class BOJ1967 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i <= n; i++) {
+            tree.add(new ArrayList<>());
+        }
+}
+}
