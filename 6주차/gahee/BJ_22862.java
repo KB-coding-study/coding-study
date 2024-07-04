3fc0723c2f528f025305fd70d1ccb60d5533b1f6
GaHee99 <77230391+GaHee99@users.noreply.github.com>
2024-07-05 07:35:00 +0900

    최가희-6주차-22862

---
+++ 6주차/gahee/BJ_22862.java
@@ -0,0 +1,41 @@
+public class BJ_22862 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] line = br.readLine().split(" ");
+        int n = Integer.parseInt(line[0]);
+        int k = Integer.parseInt(line[1]);
+        boolean[] arr = new boolean[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            arr[i] = num % 2 == 0;
+        }
+
+    
+        int maxLen = 0;
+        int l = 0;
+        int r = 0;
+        int ct = 0;
+        while (r < n) {
+            if (ct < k) {
+                if (!arr[r]) {
+                    ct++;
+                }
+                r++;
+                maxLen = Math.max(r - l - ct, maxLen);
+            } else if (arr[r]) {
+                r++;
+                maxLen = Math.max(r - l - ct, maxLen);
+            } else {
+                if (!arr[l]) {
+                    ct--;
+                }
+                l++;
+            }
+        }
+
+        System.out.println(maxLen);
+    }
+}
