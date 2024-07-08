138a202e6b94fc03c3e3d50689b906fc9b3e3a0d
GaHee99 <77230391+GaHee99@users.noreply.github.com>
2024-07-08 10:51:58 +0900

    7주차-최가희-BJ1003

---
+++ 7주차/gahee/BJ1003.java
@@ -0,0 +1,34 @@
+import java.io.*;
+
+class BJ1003 {
+    static int zero = 0;
+    static int one = 0;
+    static Integer[][] dp = new Integer[41][2];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        dp[0][0] = 1;
+        dp[0][1] = 0;
+        dp[1][0] = 0;
+        dp[1][1] = 1;
+        // 방법 1
+        for(int i=2; i<41; i++ ) {
+            dp[i][0] = dp[i-1][0] + dp[i-2][0];
+            dp[i][1] = dp[i-1][1] + dp[i-2][1];
+        }
+        for(int i=0; i<t; i++) {
+            int number = Integer.parseInt(br.readLine());
+
+            //방법 2 - 재귀
+            //  fibo(number);
+            System.out.println(dp[number][0] + " " + dp[number][1]);
+        }
+    }
+    public static Integer[] fibo(int n) {
+        if(dp[n][0]== null || dp[n][1]==null) {
+            dp[n][0] = fibo(n-1)[0] + fibo(n-2)[0];
+            dp[n][1] = fibo(n-1)[1] + fibo(n-2)[1];
+        }
+        return dp[n];
+    }
+}
