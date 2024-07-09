65ae8e847d8018e1da8bc9853299a2a7f6919a77
YeSeul <ysbsjh2464@naver.com>
2024-07-10 00:03:59 +0900

    7주차-이예슬-11727

--- 7주차/yeseul/BOJ_1932.java
+++ 7주차/yeseul/BOJ_1932.java
@@ -1,7 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class BOJ_1932 {
     public static void main(String[] args) throws IOException {
