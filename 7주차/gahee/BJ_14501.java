e6b011007081b0b646e503c0c3b7d54bba9971ba
GaHee99 <77230391+GaHee99@users.noreply.github.com>
2024-07-11 13:06:40 +0900

    7주차-최가희-14501

---
+++ 7주차/gahee/BJ_14501.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+class BJ_14501 {
+   public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] p = new int[n+2];
+        int[] d = new int[n+2];
+        long[] dp = new long[n+2];
+        for(int i=1; i<=n; i++) {
+            String [] info = br.readLine().split(" ");
+            d[i] = Integer.parseInt(info[0]);
+            p[i] = Integer.parseInt(info[1]);
+        }
+        for(int i=1; i<=n+1; i++) {
+            int day = d[i] + i;
+            dp[i] = Math.max(dp[i], dp[i - 1]);
+            if (day <= n + 1) {
+                dp[day] = Math.max(dp[day], Math.max(dp[i - 1], dp[i] + p[i]));
+            }
+        }
+       System.out.println(dp[n+1]);
+    }
+}
