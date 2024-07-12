afe17ca83f62c85af624e5468e3f83a6f2f9bc6e
YeSeul <ysbsjh2464@naver.com>
2024-07-12 16:46:24 +0900

    7주차-이예슬-15486

---
+++ 7주차/yeseul/BOJ_15486.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_15486 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] t = new int[n];
+        int[] p = new int[n];
+        int[] dp = new int[n + 1];
+
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            t[i] = Integer.parseInt(st.nextToken());
+            p[i] = Integer.parseInt(st.nextToken());
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            if (i + t[i] > n) {
+                dp[i] = dp[i + 1];
+            } else {
+                dp[i] = Math.max(dp[i+1], p[i] + dp[i + t[i]]);
+            }
+        }
+        System.out.println(dp[0]);
+    }
+}
