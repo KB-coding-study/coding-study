66603eddb6cdd12f7ad9d69e054a373fe1e9649a
YeSeul <ysbsjh2464@naver.com>
2024-07-12 17:03:32 +0900

    7주차-이예슬-15486 (주석추가)

--- 7주차/yeseul/BOJ_15486.java
+++ 7주차/yeseul/BOJ_15486.java
@@ -10,17 +10,20 @@ public class BOJ_15486 {
         int n = Integer.parseInt(br.readLine());
         int[] t = new int[n];
         int[] p = new int[n];
-        int[] dp = new int[n + 1];
+        int[] dp = new int[n + 1];  // 각 상담을 완료했을 때 받을 수 있는 금액
 
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             t[i] = Integer.parseInt(st.nextToken());
             p[i] = Integer.parseInt(st.nextToken());
         }
+
         for (int i = n - 1; i >= 0; i--) {
+            // 해당 날짜에 시작한 상담이 n일을 넘어가지 않는지 확인
             if (i + t[i] > n) {
-                dp[i] = dp[i + 1];
+                dp[i] = dp[i + 1];  // 다음날의 수익을 그대로 가져옴 => 다음날부터 탐색했으므로
             } else {
+                // (이전에 탐색한)다음날의 수익이 큰지, 상담을 할 때의 수익이 큰지 확인
                 dp[i] = Math.max(dp[i+1], p[i] + dp[i + t[i]]);
             }
         }
