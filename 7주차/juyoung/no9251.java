383a41c97c58b2d7579225cb4e985a8f0b7591b7
김주영 <77871898+kjy-asl@users.noreply.github.com>
2024-07-14 08:44:10 +0900

    7주차-김주영-9521

---
+++ 7주차/juyoung/no9251.java
@@ -0,0 +1,29 @@
+import java.io.*;
+
+public class no9521 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String str1 = reader.readLine();
+        String str2 = reader.readLine();
+
+        int m = str1.length();
+        int n = str2.length();
+
+        int[][] dp = new int[m + 1][n + 1];
+
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = 0;
+                } else if (str1.charAt(i - 1) == str2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        System.out.println(dp[m][n]);
+    }
+}
