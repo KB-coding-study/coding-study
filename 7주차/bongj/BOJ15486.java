5caad3fd72554b5c56ae129e0cabac758519d91c
bongj9 <lollypark501@naver.com>
2024-07-13 21:57:01 +0900

    7주차_장봉준_다른 방법풀이

--- 7주차/bongj/BOJ15486.java
+++ 7주차/bongj/BOJ15486.java
@@ -25,7 +25,6 @@ public class BOJ15486 {
                 continue;
             }
             benefits[i+time-1] = Math.max(benefits[i+time-1], benefits[i-1] + price);
-
             //최대값 갱신
             max = Math.max(max, benefits[i+time-1]);
         }
@@ -33,3 +32,38 @@ public class BOJ15486 {
         System.out.println(max);
     }
 }
+/*
+public class Main {
+
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int n = Integer.parseInt(br.readLine());
+		StringTokenizer st = null;
+
+		int[][] arr =new int[n+2][2];
+		int[] dp = new int[n+2];
+		for(int i=1; i<n+1; i++) {
+			st = new StringTokenizer(br.readLine());
+
+			int t = Integer.parseInt(st.nextToken());
+			int p = Integer.parseInt(st.nextToken());
+			arr[i][0] = t; // 기간
+			arr[i][1] = p; // 금액
+		}
+
+		int max = -1;
+		for(int i=1; i<=n+1; i++) {
+			if(max < dp[i]) {
+				max = dp[i];
+			}
+
+			int nxt = i +arr[i][0];
+			if(nxt < n+2) {
+				dp[nxt] = Math.max(dp[nxt], max+arr[i][1]);
+			}
+		}
+		System.out.println(dp[n+1]);
+	}
+}
+ */
