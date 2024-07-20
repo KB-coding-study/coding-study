e7292ad6e475537634c58da9347890f2def47e71
ingyeom <wjd1836@gmail.com>
2024-07-21 01:22:02 +0900

    8주차-정인겸-9655,2011

---
+++ 8주차/ingyeom/BOJ_9655.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(n % 2 == 1 ? "SK" : "CY");
+        bw.close();
+    }
+}
