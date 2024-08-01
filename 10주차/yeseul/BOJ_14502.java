828764722429b7dc7094668069087cf27692ac0a
YeSeul <ysbsjh2464@naver.com>
2024-08-02 00:43:40 +0900

    10주차-이예슬-14502(수정)

--- 10주차/yeseul/BOJ_14502.java
+++ 10주차/yeseul/BOJ_14502.java
@@ -2,43 +2,47 @@ import java.io.*;
 import java.util.*;
 
 public class BOJ_14502 {
-
     // 0 => 빈 칸, 1 => 벽, 2 => 바이러스
     // 벽 3개 세우기 필수
-    static int N, M, result, max;
+    static int n, m, max;
+    static int[][] map;
+    static int[][] tmpMap;
+    static boolean[][] spreadVisited;
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
-    static int[][] map, tmp;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        map = new int[N][M];
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        map = new int[n][m];
+        tmpMap = new int[n][m];
 
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
+            for (int j = 0; j < m; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        max = 0;
+        max = Integer.MIN_VALUE;
         dfs(0);
+
         System.out.println(max);
     }
 
-    // 아무 곳이나 벽 3개 세우기
+    // 벽 세우기
     static void dfs(int cnt) {
+        // 벽을 3개 세웠을 때
         if (cnt == 3) {
-            countArea();
+            getSafeArea();
             return;
         }
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 if (map[i][j] == 0) {
                     map[i][j] = 1;
                     dfs(cnt + 1);
@@ -46,16 +50,33 @@ public class BOJ_14502 {
                 }
             }
         }
+
     }
 
-    // 안전구역 세기
-    static void countArea() {
-        bfs();
+    // 안전 영역 구하기
+    static void getSafeArea() {
+        // map 배열 임시 복사
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                tmpMap[i][j] = map[i][j];
+            }
+        }
 
-        result = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (tmp[i][j] == 0) {
+        // 바이러스 퍼지게 하기
+        spreadVisited = new boolean[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (tmpMap[i][j] == 2 && !spreadVisited[i][j]) {
+                    spreadVirus(i, j);
+                }
+            }
+        }
+
+        // 최종 map에서 안전 영역 계산
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (tmpMap[i][j] == 0) {
                     result++;
                 }
             }
@@ -63,35 +84,26 @@ public class BOJ_14502 {
         max = Math.max(max, result);
     }
 
-    // 바이러스 퍼지기
-    static void bfs() {
-        Queue<int[]> queue = new LinkedList<>();
-        tmp = new int[N][M];
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                tmp[i][j] = map[i][j];
-            }
-        }
+    // 바이러스 퍼지게 하기
+    static void spreadVirus(int x, int y) {
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{x, y});
+        spreadVisited[x][y] = true;
 
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (tmp[i][j] == 2) {
-                    queue.offer(new int[]{i, j});
-                }
-            }
-        }
-
-        while (!queue.isEmpty()) {
-            int[] xy = queue.poll();
+        while (!q.isEmpty()) {
+            int[] xy = q.poll();
 
             for (int i = 0; i < 4; i++) {
-                int nx = xy[0] + dx[i];
-                int ny = xy[1] + dy[i];
+                int nx = dx[i] + xy[0];
+                int ny = dy[i] + xy[1];
 
-                if (nx >= 0 && ny >= 0 && nx < N && ny < M && tmp[nx][ny] == 0) {
-                    tmp[nx][ny] = 2;
-                    queue.offer(new int[]{nx, ny});
-                }
+                if (nx >= n || ny >= m || nx < 0 || ny < 0) continue;
+                if (spreadVisited[nx][ny]) continue;
+                if (tmpMap[nx][ny] == 1) continue;
+
+                tmpMap[nx][ny] = 2;
+                spreadVisited[nx][ny] = true;
+                q.offer(new int[]{nx, ny});
             }
         }
     }
